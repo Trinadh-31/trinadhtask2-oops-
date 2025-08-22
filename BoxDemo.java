5a83623b815ff4b139853a37bc5e144ca26f3cc7
Trinadh-31 <trinadh.123v@gmail.com>
2025-08-22 21:05:36 +0530

    Create BoxDemo.java

---
+++ BoxDemo.java
@@ -0,0 +1,26 @@
+package starsprinting;
+
+class Box
+{
+    double width;
+    double height; 
+    double depth; 
+} 
+
+class BoxDemo 
+{
+    public static void main(String args[])
+     {  
+	Box mybox = new Box(); 
+	double vol; 
+
+	
+	mybox.width = 10;
+	mybox.height = 15; 
+	mybox.depth = 15; 
+
+	
+	 vol = mybox.width * mybox.height * mybox.depth; 	
+	 System.out.println("Volume is " + vol); 
+        } 
+} 
