9c31c3f9c0a7af085d5ff64b3bcc3bc8f99e4009
Trinadh-31 <trinadh.123v@gmail.com>
2025-08-22 21:10:45 +0530

    Create implicity.java

---
+++ implicity.java
@@ -0,0 +1,13 @@
+package starsprinting;
+
+
+public class implicity {
+	public static void main(String[] args) {
+		byte b=10;
+		int c;
+		c=b;
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
