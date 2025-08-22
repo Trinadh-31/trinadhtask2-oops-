40732f5756c1802f16e02e38a1a036c078735b0a
Trinadh-31 <trinadh.123v@gmail.com>
2025-08-22 21:06:12 +0530

    Create Buffer2.java

---
+++ Buffer2.java
@@ -0,0 +1,10 @@
+package starsprinting;
+
+public class Buffer2 {
+	public static void main(String[] args) {
+		StringBuffer a=new StringBuffer("sanjay");
+		a.replace(4,6,"uu");
+		System.out.println(a);
+	}
+
+}
