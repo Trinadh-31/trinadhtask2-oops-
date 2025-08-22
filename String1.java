ee2791b3f3c8f7d67cc7fcdf7314f35467586585
Trinadh-31 <trinadh.123v@gmail.com>
2025-08-22 21:09:49 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,24 @@
+package starsprinting;
+
+public class String1{
+
+	  public static void main(String[] args) 
+	  {
+	    String s1="trinadh";
+	    String s2="vatyam";
+	    System.out.println(s1==s2);
+	    
+	    s1=s1+"koundinya";
+	    System.out.println(s1==s2);
+	    
+	    String s3="trinadh";
+	    
+	    
+	    System.out.println(s1==s3);
+	    
+
+	  }
+
+	
+
+}
